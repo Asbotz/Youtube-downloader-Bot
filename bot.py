c0bb726940dba1af244751a99ea54064b088cd8f
Asbotz <144653726+Asbotz@users.noreply.github.com>
2023-10-30 19:29:29 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,9 +5,10 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from datetime import datetime, timedelta
 
 # Add your API ID, API Hash, and Bot Token here
-api_id = 'YOUR_API_ID'
-api_hash = 'YOUR_API_HASH'
-bot_token = 'YOUR_BOT_TOKEN'
+api_id = '20191141'
+api_hash = '059da8863312a9bdf1fa04ec3467a528'
+bot_token = '6008466751:AAFjUsWB-wAvc04004E7f7STbNql5QphKEI'  # Replace with your bot token
+
 
 # Initialize the Pyrogram client
 app = Client("url_uploader_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
