d1abd666f58743ec16de535c86a6f3158e27808a
Asbotz <144653726+Asbotz@users.noreply.github.com>
2023-10-31 17:23:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ import os
 # Your API credentials
 api_id = '20191141'
 api_hash = '059da8863312a9bdf1fa04ec3467a528'
-bot_token = '5947039016:AAEE3h7ezQoFNkngnrFom5KiTlU4g9iUdr0'
+bot_token = '6759465412:AAFAxePYnXgIOT2ZdD4T71KyLxXigr7iWXc'
 
 # Create a Pyrogram Client instance
 app = Client("url_uploader_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
